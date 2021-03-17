e1637788ef3070bda5a0df50f2b8891d3c03aa26
Muhammed Can Keleş <52157220+CanKeles5@users.noreply.github.com>
2021-03-17 22:11:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # coding: utf-8
 import os
 
-datasets_root = '/home'
+datasets_root = '/content/dataroot'
 
 # For each dataset, I put images and masks together
 ecssd_path = os.path.join(datasets_root, 'ecssd')
 hkuis_path = os.path.join(datasets_root, 'hkuis')
 pascals_path = os.path.join(datasets_root, 'pascals')
 dutomron_path = os.path.join(datasets_root, 'dutomron')
-duts_path = os.path.join(datasets_root, 'duts')
-duts_train_path = os.path.join(datasets_root, 'duts_train')
+duts_path = os.path.join(datasets_root, '')
+duts_train_path = os.path.join(datasets_root, 'train')
 sod_path = os.path.join(datasets_root, 'sod')
