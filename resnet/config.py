d5d25a49b9f3e50c299e16647e511f0fdc88b63e
Muhammed Can Keleş <52157220+CanKeles5@users.noreply.github.com>
2021-03-17 22:14:24 +0300

    Update config.py

--- resnet/config.py
+++ resnet/config.py
@@ -1,4 +1,2 @@
 import os
-pytorch_pretrained_root = '/home/b3-542/Packages/Models/PyTorch Pretrained'
-pretrained_res50_path = os.path.join(pytorch_pretrained_root, 'ResNet', 'resnet50-19c8e357.pth')
-
+pretrained_res50_path = "/content/DPNet/resnet50-19c8e357.pth"
